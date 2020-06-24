4b910e7fcb8a5e06989bf3d7c2fb93d5fec6921f
Dennis Wollersheim <dewoller@gmail.com>
2020-06-24 21:54:13 +1000

    workflowr::wflow_publish("R/plan.R")

--- R/plan.R
+++ R/plan.R
@@ -1,3 +1,10 @@
+# age on x axis(
+#
+# 3.  can remove outliers >2.5 sd from mean  try removeing outliers that are 2.5 and 3, rerun graphs
+# display confidence intervals for overall graph
+# get rid of all background, and white grid
+
+
 min_mandndi = 80
 prefix='cog_gml'  # all relevant variables start with this
 
@@ -5,13 +12,14 @@ the_plan <-
   drake_plan(
    ## Plan targets in here.
              spss_orig  = read.spss('data/Groton Maze_v2.sav') ,
-             df_in = process_input_data(prefix, spss_orig, min_mandndi )  ,
+             df_in_long = process_input_data(prefix, spss_orig, min_mandndi )  ,
              df_one_names = get_spss_names( spss_orig )  ,
-             df_model = generate_all_models( df_in ),
-             makeModel = target(
+             df_model = generate_all_models( df_in_long ),
+             graphs = target(
                                   command = {
-                                    rmarkdown::render(knitr_in("analysis/models.Rmd"))
-                                    file_out("doc/models.html")
+                                    knitr_in("R/make_one_graph_set.R")
+                                    workflowr::wflow_build( knitr_in("analysis/initial.Rmd"))
+                                    file_out("docs/initial.html")
                                   }
              )
 )
